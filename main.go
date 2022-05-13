331d874e6a49505dcbfafb0b2e40c156dd7145d6
nkt <pauloxti@gmail.com>
2022-05-13 01:53:52 +0100

    refactor

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
     "fmt"
+    "github.com/nikola43/getsysteminformationgolang/sysinfo"
 )
 
 func main() {
-    info := newSysInfo()
+    info := sysinfo.newSysInfo()
     fmt.Printf("%+v\n", info)
 	fmt.Printf("%+s\n", info.toString())
 	fmt.Printf("%+s\n", info.toHash())
